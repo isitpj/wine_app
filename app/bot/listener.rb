4ebe618499e7be30eeb1a3e384985b8357f10e3f
Peter Johnstone <peter.wdj@gmail.com>
2018-06-03 21:02:32 +0100

    Wraps in a named Listener class

--- app/bot/listener.rb
+++ app/bot/listener.rb
@@ -2,12 +2,14 @@ require 'facebook/messenger'
 
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["FB_ACCESS_TOKEN"])
-Bot.on :message do |message|
-  Bot.deliver({
+class Listener
+  Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["FB_ACCESS_TOKEN"])
+  Bot.on :message do |message|
+    Bot.deliver({
       recipient: message.sender,
       message: {
         text: 'Hey there, Peter.'
       }
     }, access_token: ENV['FB_ACCESS_TOKEN'])
+  end
 end
