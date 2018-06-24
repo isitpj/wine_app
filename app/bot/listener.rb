4864ce2ac07006c1bf82b5b40840206c8bccfa28
Peter Johnstone <peter.wdj@gmail.com>
2018-06-24 22:22:54 +0100

    Adds ability to handle user requesting to add a bot of red.

--- app/bot/listener.rb
+++ app/bot/listener.rb
@@ -5,11 +5,21 @@ include Facebook::Messenger
 class Listener
   Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["FB_ACCESS_TOKEN"])
   Bot.on :message do |message|
-    Bot.deliver({
-      recipient: message.sender,
-      message: {
-        text: 'Hey there, Peter.'
-      }
-    }, access_token: ENV['FB_ACCESS_TOKEN'])
+    sender = message.sender
+    if /red/i.match? message.text
+      Bot.deliver({
+        recipient: sender,
+        message: {
+          text: 'How lovely! Would you like to add a new bottle of red to your cellar?'
+        }
+        }, access_token: ENV['FB_ACCESS_TOKEN'])
+    else
+      Bot.deliver({
+        recipient: sender,
+        message: {
+          text: "Hey there, #{sender}."
+        }
+        }, access_token: ENV['FB_ACCESS_TOKEN'])
+    end
   end
 end
