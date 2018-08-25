2f542c38509ae72b7b2dcdd96051f66570c4272a
Peter Johnstone <peter.wdj@gmail.com>
2018-08-25 18:37:09 +0100

    Update test expectation matchers to get tests to pass; big refactor of lots of test code.

--- spec/bot/listener_spec.rb
+++ spec/bot/listener_spec.rb
@@ -5,22 +5,7 @@ RSpec.describe Listener do
   FakeMessage = Struct.new(:sender, :recipient, :timestamp, :text, :quick_replies)
 
   before(:each) do
-    response_data = {
-      "first_name" => "Peter",
-      "last_name" => "Johnstone",
-      "profile_pic" => "https://platform-lookaside.fbsbx.com/platform/profilepic/",
-      "id" => "123456"
-    }.to_json
-
-    stub_request(:get, "https://graph.facebook.com/1234?access_token=#{ENV['FB_ACCESS_TOKEN']}&fields=first_name,last_name,profile_pic").
-         with(
-           headers: {
-       	  'Accept'=>'*/*',
-       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	  'Host'=>'graph.facebook.com',
-       	  'User-Agent'=>'Ruby'
-           }).
-         to_return(status: 200, body: response_data, headers: {})
+    stub_facebook_user_data_request
   end
 
   describe 'Bot#on(message)' do
@@ -33,25 +18,22 @@ RSpec.describe Listener do
     it 'invites the user to add a new bottle of red wine' do
       user_message = fake_message('I just had a bottle of red')
       expected_response = 'How lovely! Would you like to add a new bottle of red to your cellar?'
-      expect_bot_message_to_contain(user_message, expected_response)
 
-      Bot.trigger(:message, user_message)
+      expect_bot_message_to_have_text(user_message, expected_response)
     end
 
     it 'invites the user to add a new bottle of white wine' do
       user_message = fake_message('I just had a bottle of white')
       expected_response = 'How lovely! Would you like to add a new bottle of white to your cellar?'
-      expect_bot_message_to_contain(user_message, expected_response)
 
-      Bot.trigger(:message, user_message)
+      expect_bot_message_to_have_text(user_message, expected_response)
     end
 
     it 'makes a GET request to facebook to retrieve profile information' do
       user_message = fake_message('Hey bot')
-      string = "https://graph.facebook.com/#{user_message.sender['id']}?fields=first_name,last_name,profile_pic&access_token=#{ENV["FB_ACCESS_TOKEN"]}"
 
       allow(Bot).to receive(:deliver) {}
-      expect(URI).to receive(:parse)
+      expect(URI).to receive(:parse).with(facebook_user_data_request_url)
       expect(Net::HTTP).to receive(:get) { "{\"first_name\":\"Peter\",\"last_name\":\"Johnstone\"}" }
 
       Bot.trigger(:message, user_message)
@@ -66,55 +48,71 @@ RSpec.describe Listener do
       }
       user_message = fake_message(message, quick_replies: quick_reply)
 
-      ap '==========================='
-      ap user_message
-      ap '==========================='
-
       expected_response = 'Would you like to create your account with Charles d\'Née?'
+
       expect_bot_message_to_have_text(user_message, expected_response)
       expect_bot_message_to_have_quick_reply(user_message, quick_reply)
-
-
-      # expect_bot_message_to_contain(user_message, expected_response)
-
-      Bot.trigger(:message, user_message)
     end
   end
 
   private
 
-  def fake_message(message_text, quick_replies: {})
+  def fake_message(message_text, quick_replies: [])
     sender = {"id"=>"1234"}
     recipient = {"id"=>"5678"}
     timestamp = 1528049653543
-    quick_replies = [{
-      content_type: quick_replies[:content_type],
-      title: quick_replies[:title],
-      payload: quick_replies[:payload]
-    }]
+    quick_replies = quick_replies
     FakeMessage.new(sender, recipient, timestamp, message_text, quick_replies)
-  end
-
-  def expect_bot_message_to_contain(message, text)
-    expect(Bot).to receive(:deliver).with({
-      recipient: message.sender,
-      message: {
-        text: text
-      }
-      }, access_token: ENV['FB_ACCESS_TOKEN'])
   end
 
   def expect_bot_message_to_have_text(message, text)
     expect(Bot).to receive(:deliver)
-    expect(message.text).to eq(text)
+      .with(
+        hash_including(message: hash_including(text: text)),
+        access_token: ENV['FB_ACCESS_TOKEN']
+      )
+
+      Bot.trigger(:message, message)
   end
 
   def expect_bot_message_to_have_quick_reply(message, quick_reply)
     expect(Bot).to receive(:deliver)
-    message.quick_replies.any? do |reply|
-      expect(reply[:content_type]).to eq(quick_reply[:content_type])
-      expect(reply[:title]).to eq(quick_reply[:title])
-      expect(reply[:payload]).to eq(quick_reply[:payload])
-    end
+      .with(
+        hash_including(
+          message: hash_including(
+            quick_replies: array_including(quick_reply)
+          )
+        ),
+        access_token: ENV['FB_ACCESS_TOKEN']
+      )
+
+      Bot.trigger(:message, message)
+  end
+
+  def stub_facebook_user_data_request
+    stub_request(:get, facebook_user_data_request_url)
+      .with(facebook_user_data_request_headers)
+      .to_return(facebook_user_data_response)
+  end
+
+  def facebook_user_data_request_url
+    "https://graph.facebook.com/1234?fields=first_name,last_name,profile_pic&access_token=#{ENV['FB_ACCESS_TOKEN']}"
+  end
+
+  def facebook_user_data_request_headers
+    {headers: {'Accept'=>'*/*'}}
+  end
+
+  def facebook_user_data_response
+    {
+      status: 200,
+      body: {
+        "first_name" => "Peter",
+        "last_name" => "Johnstone",
+        "profile_pic" => "https://platform-lookaside.fbsbx.com/platform/profilepic/",
+        "id" => "1234"
+      }.to_json,
+      headers: {}
+    }
   end
 end
