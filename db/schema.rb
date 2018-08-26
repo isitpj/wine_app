d11770c7f0aa279f6f94fddcefb4356840f5fe9a
Peter Johnstone <peter.wdj@gmail.com>
2018-08-26 14:46:45 +0100

    DB migration - limits integer on User#facebook_id to 8 bytes instead of 4.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180826125945) do
+ActiveRecord::Schema.define(version: 20180826133137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20180826125945) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.integer "facebook_id"
+    t.bigint "facebook_id"
     t.text "profile_pic_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
