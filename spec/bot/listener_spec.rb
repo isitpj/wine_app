3a9c0b9f89afac5668aa74f27b3c9769a570f62b
Peter Johnstone <peter.wdj@gmail.com>
2018-08-27 17:17:43 +0100

    Minor refactor of fake_message private method.

--- spec/bot/listener_spec.rb
+++ spec/bot/listener_spec.rb
@@ -19,16 +19,16 @@ RSpec.describe Listener do
     recipient = {"id"=>"5678"}
     timestamp = 1528049653543
     messaging = {
-      "sender"=>{"id"=>"1732016540208841"},
-        "recipient"=>{"id"=>"364376550736984"},
-        "timestamp"=>1535288528163,
+      "sender"=>sender,
+        "recipient"=>recipient,
+        "timestamp"=>timestamp,
         "message"=>{
-          "quick_reply"=>{"payload"=>quick_reply_payload},
           "mid"=> "0j3SQeNnpIDLxBwJl7hRofACd_36bJGN3qXKXv32Bok8GqJfA284e1hsnOagFiVZsbqLLainVGIVURWOlNJ4Tw",
           "seq"=>2171281,
-          "text"=>"Account"
+          "text"=>message_text
         }
       }
+      messaging['message']['quick_reply'] = {"payload"=>quick_reply_payload} if quick_reply_payload
     FakeMessage.new(sender, recipient, timestamp, message_text, messaging)
   end
 end
