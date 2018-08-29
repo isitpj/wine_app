2f01a945ad882443021b9c99c65db6aea8244d42
Peter Johnstone <peter.wdj@gmail.com>
2018-08-29 21:49:18 +0100

    Adds test to ensure the returned data is parsed.

--- spec/services/users/retrieve_user_data_spec.rb
+++ spec/services/users/retrieve_user_data_spec.rb
@@ -4,9 +4,16 @@ RSpec.describe Users::RetrieveUserData do
   describe '#call' do
     it 'makes a GET request to retrieve a user\'s data' do
       expect(URI).to receive(:parse).with(facebook_user_data_request_url)
-      expect(Net::HTTP).to receive(:get)
+      expect(Net::HTTP).to receive(:get) { facebook_user_data_response }
 
       Users::RetrieveUserData.call(fake_user_facebook_id)
+    end
+
+    it 'parses the returned JSON object and returns a hash' do
+      expect(JSON).to receive(:parse)
+      allow_any_instance_of(Users::RetrieveUserData).to receive(:get_facebook_data) { facebook_user_data_response }
+
+      data = Users::RetrieveUserData.call(fake_user_facebook_id)
     end
   end
 
@@ -18,5 +25,18 @@ RSpec.describe Users::RetrieveUserData do
 
   def fake_user_facebook_id
     1234
+  end
+
+  def facebook_user_data_response
+    {
+      status: 200,
+      body: {
+        "first_name" => "Peter",
+        "last_name" => "Johnstone",
+        "profile_pic" => "https://platform-lookaside.fbsbx.com/platform/profilepic/",
+        "id" => "1234"
+      }.to_json,
+      headers: {}
+    }.to_json
   end
 end
