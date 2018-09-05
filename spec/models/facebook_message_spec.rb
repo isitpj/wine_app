5341709bb084d70b8fd2391f2cd66e3e5df260cd
Peter Johnstone <peter.wdj@gmail.com>
2018-09-05 20:27:18 +0100

    Adds tests on validations.

--- spec/models/facebook_message_spec.rb
+++ spec/models/facebook_message_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe FacebookMessage, type: :model do
-  describe 'basics' do
+  describe 'validations' do
     it 'must have a name' do
-      message = create(:facebook_message)
+      message = build(:facebook_message, name: nil, body: 'Some text') # nil assignment overrides factory
 
-      ap message
+      expect(message).to_not be_valid
+    end
+
+    it 'must have a category' do
+      message = build(:facebook_message, category: nil, body: 'Some text') # nil assignment overrides factory
+
+      expect(message).to_not be_valid
+    end
+
+    it 'must have a body' do
+      message = build(:facebook_message)
+
+      expect(message).to_not be_valid
     end
   end
 end
