f787cd2ae3b9e614fcfce70a200b7d07dccb4e1d
Peter Johnstone <peter.wdj@gmail.com>
2018-10-24 21:01:51 +0100

    IT'S MAKING SOME API CALLS HELL FUCKING YEAH

--- client/src/components/Messages.vue
+++ client/src/components/Messages.vue
@@ -9,70 +9,26 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      messages: [
-        {
-          "id": 1,
-          "name": "Fallback Message",
-          "category": "fallback",
-          "body": "I'm sorry, I don't understand that yet",
-          "quick_replies": null,
-          "buttons": null,
-          "created_at": "2018-10-03T20:28:31.136Z",
-          "updated_at": "2018-10-03T20:28:31.136Z"
-        }
-      ]
-    }
-  }
-}
-</script>
-
-<style lang="css">
-</style>
-
-
-<!-- <template>
-  <div id="messages">
-    <div v-for="(message) in messages" :key="message.id">
-      <p>{{message.name}}</p>
-      <p>Text: {{message.body}}</p>
-      <p>Category: {{message.category}}</p>
-    </div>
-  </div>
-</template>
-
-<script>
 import axios from 'axios'
 export default {
   data() {
     return {
       messages: [],
-      name: [],
-      relationship: [],
-      errors: []
+      errors: [],
     }
-   },
-   created() {
-    axios.get('http://localhost:3000/api/messages',
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        responseType: 'json',
+  },
+  created() {
+    axios.get('http://localhost:3000/api/messages')
+      .then(response => {
+        this.messages = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
       }
-      },
     )
-    .then(response => {
-      this.messages = response.data
-    })
-    .catch(e => {
-      this.error.push(e)
-    })
   }
 }
 </script>
 
 <style lang="css">
 </style>
- -->
