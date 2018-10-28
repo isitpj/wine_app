b88ce79809bd1eb6cc23ec572f835f6c5bb86806
Peter Johnstone <peter.wdj@gmail.com>
2018-10-28 23:45:50 +0000

    Creates NewMessageForm component to render a form to create a new message.

---
+++ client/src/components/messages/NewMessageForm.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <form class="" action="https://localhost:3000/api/message" method="post">
+    <input type="text" name="message-name" value="Enter a name for the message">
+    <select name="category">
+      <option value="fallback">Fallback</option>
+    </select>
+    <textarea name="message-text" rows="4" cols="80">Enter the text for the message here...</textarea>
+    <input type="submit" value="Create message">
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'newMessageForm'
+}
+</script>
+
+<style lang="css">
+</style>
