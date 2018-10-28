51ec3be2039645feaf8ff5a09859e32d1d940c17
Peter Johnstone <peter.wdj@gmail.com>
2018-10-28 23:43:33 +0000

    Replaces specific OpenModalButton with scalable Button component that emits events up to parent component.

---
+++ client/src/components/Button.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <button
+    type="button"
+    class="btn"
+    @click="action"
+  >
+  {{ copy }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'buttonComponent',
+  props: ['copy'],
+  methods: {
+    action() {
+      this.$emit('action');
+    }
+  },
+}
+</script>
+
+<style lang="css">
+</style>
