6f1e1eb10ab490479b6aa095882eba9c232023ae
Peter Johnstone <peter.wdj@gmail.com>
2018-11-06 21:30:28 +0000

    Adds create route for FacebookMessage.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Facebook::Messenger::Server, at: 'bot'
   namespace :api, defaults: {format: :json} do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
 end
