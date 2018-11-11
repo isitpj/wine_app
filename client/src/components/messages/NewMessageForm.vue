46932fef2094287b18e6a3f22c0892ebd073a238
Peter Johnstone <peter.wdj@gmail.com>
2018-11-11 22:17:48 +0000

    Creates post request to create a FacebookMessage with hard-coded parameters. Todo: get these params from the form.

--- client/src/components/messages/NewMessageForm.vue
+++ client/src/components/messages/NewMessageForm.vue
@@ -5,13 +5,37 @@
       <option value="fallback">Fallback</option>
     </select>
     <textarea name="message-text" rows="4" cols="80" placeholder="Enter the text for the message here..."></textarea>
-    <input type="submit" value="Create message">
+    <!-- <input type="submit" value="Create message"> -->
+    <button
+      type="submit"
+      v-on:click.prevent="createMessage"
+    >
+      Create
+    </button>
   </form>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'newMessageForm'
+  name: 'newMessageForm',
+  methods: {
+    createMessage: function() {
+      axios.post('http://localhost:3000/api/messages', {
+        message: {
+          name: 'Test message',
+          category: 'fallback',
+          body: 'Hello, world'
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  },
 }
 </script>
 
