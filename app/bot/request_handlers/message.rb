99d5200a4ee39923f8ec704fb6631c30d14c60c4
Peter Johnstone <peter.wdj@gmail.com>
2018-11-11 22:18:54 +0000

    Moves Message.rb out of RequestHandlers module to prevent namespacing bug.

--- app/bot/request_handlers/message.rb
+++ app/bot/request_handlers/message.rb
@@ -1,11 +1,9 @@
-module RequestHandlers
-  class Message
-    def self.handle(message)
-      intent = Intents::Classifier.classify(message)
-      response_message = Intents::Mapper.map_intent_to_message(intent)
-      response_message[:recipient] = message.sender
+class Message
+  def self.handle(message)
+    intent = Intents::Classifier.classify(message)
+    response_message = Intents::Mapper.map_intent_to_message(intent)
+    response_message[:recipient] = message.sender
 
-      Bot.deliver(response_message, access_token: ENV['ACCESS_TOKEN'])
-    end
+    Bot.deliver(response_message, access_token: ENV['ACCESS_TOKEN'])
   end
 end
