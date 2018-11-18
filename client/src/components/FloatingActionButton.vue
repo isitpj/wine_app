aaf865b8e2ec1101e12a69af253ce96b596c1528
Peter Johnstone <peter.wdj@gmail.com>
2018-11-18 21:02:45 +0000

    Creates new FAB component.

---
+++ client/src/components/FloatingActionButton.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <button
+    type="button"
+    class="floating-action-button"
+    @click="action"
+  >
+    <span class="material-icons">{{materialIconTag}}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'FloatingActionButton',
+  props: ['materialIconTag'],
+  methods: {
+    action() {
+      this.$emit('action');
+    }
+  },
+}
+</script>
+
+<style lang="css">
+.floating-action-button {
+  height: 6em;
+  width: 6em;
+  border-radius: 5em;
+  position: fixed;
+  right: 2em;
+  bottom: 2em;
+  border-style: none;
+  background-color: #ff0083;
+  box-shadow: 3px 3px grey;
+}
+
+.material-icons {
+  color: #ffffff;
+  font-size: 4em;
+  font-weight: 100;
+}
+</style>
